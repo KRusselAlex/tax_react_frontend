d95765ba8f65244555b68d2022845f0cb125416f
KAlexRussel <kouawoualex1234@gmail.com>
2025-03-14 07:53:23 +0100

    create react project woth vite and i8 for translation and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-center text-red-400'>
+        bojour cest alex
+      </div>
+    </>
+  )
+}
+
+export default App
